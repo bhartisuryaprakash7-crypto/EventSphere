4e5c680a813b3a64ea061b215050295df2b89a7e
bhartisuryaprakash7-crypto <bhartisuryaprakash7@gmail.com>
2026-06-27 12:37:59 +0530

    updated

--- server/src/app.js
+++ server/src/app.js
@@ -21,13 +21,22 @@ const app = express();
 
 // ── Middleware ────────────────────────────────────────────────
 app.use(helmet());
+// app.use(cors({
+//   origin: [
+//     process.env.CLIENT_URL,
+//     'http://localhost:5173'
+//   ],
+//   credentials: true
+// }));
+
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL,
+    /\.vercel\.app$/,
     'http://localhost:5173'
   ],
   credentials: true
 }));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
