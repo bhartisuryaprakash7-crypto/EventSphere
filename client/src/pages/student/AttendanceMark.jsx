4fa75534037170ad15df1f351f583d097547f72e
bhartisuryaprakash7-crypto <bhartisuryaprakash7@gmail.com>
2026-06-27 11:58:02 +0530

    fix: correct api import path in AttendanceMark

--- client/src/pages/student/AttendanceMark.jsx
+++ client/src/pages/student/AttendanceMark.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import api from '../../../services/api';
+import api from '../../services/api';
 
 const AttendanceMark = () => {
   const { eventId } = useParams();
